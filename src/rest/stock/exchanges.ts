6f7ab1e433272c863c8c78e6ce6953c35e66798d
Julien Prugne <julien.prugne@gmail.com>
2019-10-15 13:29:40 +0200

    [STOCK] stock/exchanges

---
+++ src/rest/stock/exchanges.ts
@@ -0,0 +1,13 @@
+// CF: https://polygon.io/docs/#!/Stocks--Equities/get_v1_meta_exchanges
+import { get } from "../transport/request";
+
+export interface IExchange {
+  id: number;
+  type: string;
+  market: string;
+  mic: string;
+  name: string;
+  tape: string;
+}
+
+export const exchanges = (): Promise<IExchange[]> => get(`/v1/meta/exchanges`);
--- src/rest/stock/index.test.ts
+++ src/rest/stock/index.test.ts
@@ -2,6 +2,7 @@ import * as sinon from "sinon";
 import * as chai from "chai";
 
 import * as request from "../transport/request";
+import { exchanges } from ".";
 
 describe("[REST] Stock / equities", () => {
   chai.should();
@@ -12,5 +13,11 @@ describe("[REST] Stock / equities", () => {
   });
   afterEach(() => {
     sandbox.restore();
+  });
+
+  it("exchanges call /v1/meta/exchanges", async () => {
+    await exchanges();
+    requestStub.callCount.should.eql(1);
+    requestStub.getCalls()[0].args[0].should.eql("/v1/meta/exchanges");
   });
 });
