4196ebf572c172d54c15bc9e9872b9e0691d224a
Julien Prugne <julien.prugne@gmail.com>
2019-10-16 05:57:17 +0200

    [TEST] forex / aggregates

--- src/rest/forex/index.test.ts
+++ src/rest/forex/index.test.ts
@@ -1,7 +1,7 @@
 import * as sinon from "sinon";
 import * as chai from "chai";
-
 import * as request from "../transport/request";
+import { forexAggregates, forexGroupedDaily, forexPreviousClose } from ".";
 
 describe("[REST] Forex / Currencies", () => {
   chai.should();
@@ -12,5 +12,29 @@ describe("[REST] Forex / Currencies", () => {
   });
   afterEach(() => {
     sandbox.restore();
+  });
+
+  it("forexPreviousClose call /v2/aggs/ticker/{ticker}/prev", async () => {
+    await forexPreviousClose("EURCHF");
+    requestStub.callCount.should.eql(1);
+    requestStub.getCalls()[0].args[0].should.eql("/v2/aggs/ticker/EURCHF/prev");
+  });
+
+  it("forexAggregates call /v2/aggs/ticker/{ticker}/range/{multiplier}/{timespan}/{from}/{to}", async () => {
+    await forexAggregates("EURCHF", 1, "day", "2019-01-01", "2019-02-01");
+    requestStub.callCount.should.eql(1);
+    requestStub
+      .getCalls()[0]
+      .args[0].should.eql(
+        "/v2/aggs/ticker/EURCHF/range/1/day/2019-01-01/2019-02-01"
+      );
+  });
+
+  it("forexGroupedDaily call /v2/aggs/grouped/locale/{locale}/market/{market}/{date}", async () => {
+    await forexGroupedDaily("US", "FX", "2019-02-01");
+    requestStub.callCount.should.eql(1);
+    requestStub
+      .getCalls()[0]
+      .args[0].should.eql("/v2/aggs/grouped/locale/US/market/FX/2019-02-01");
   });
 });
