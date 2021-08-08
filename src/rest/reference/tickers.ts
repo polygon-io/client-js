3c4b2712e02a751da88509bc1d50b28dadae2148
Sardorkhuja Tukhtakhodjaev <34943401+Khuzha@users.noreply.github.com>
2021-08-08 18:53:32 +0500

    fix ITicker interface

--- src/rest/reference/tickers.ts
+++ src/rest/reference/tickers.ts
@@ -1,36 +1,34 @@
-// CF: https://polygon.io/docs/#!/Reference/get_v2_reference_tickers
-import { get, IPolygonQuery } from "../transport/request";
+import { IPolygonQuery } from "../transport/request";
 
 export interface ITickersQuery extends IPolygonQuery {
-  sort?: string;
-  type?: string;
-  market?: string;
-  locale?: string;
-  search?: string;
-  perpage?: number;
-  page?: number;
-  active?: boolean;
+    sort?: string;
+    type?: string;
+    market?: string;
+    locale?: string;
+    search?: string;
+    perpage?: number;
+    page?: number;
+    active?: boolean;
 }
 
-export interface ITickers {
-  ticker: string;
-  name: string;
-  market: string;
-  locale: string;
-  currency: string;
-  active: string;
-  primaryExch: string;
-  type: string;
-  codes: { [key: string]: string };
-  updated: string;
-  url: string;
+export interface ITicker {
+    ticker: string;
+    name: string;
+    market: string;
+    locale: string;
+    currency_name: string;
+    active: boolean;
+    primary_exchange: string;
+    cik: string;
+    composite_figi: string;
+    share_class_figi: string;
+    last_updated_utc: string;
+    type: string;
+    codes?: { [key: string]: string };
+    updated: string;
+    url: string;
 }
 
-export const tickers = async (
-  apiKey: string,
-  apiBase: string,
-  query?: ITickersQuery
-): Promise<ITickers[]> => {
-  const path: string = "/v3/reference/tickers";
-  return get(path, apiKey, apiBase, query);
-};
+export type ITickers = Array<ITicker>;
+
+export declare const tickers: (apiKey: string, apiBase: string, query?: ITickersQuery) => Promise<ITickers>;
