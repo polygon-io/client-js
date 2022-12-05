22e67a4e25921d23643baed68c760129441cbdb2
mpatel18 <36168387+mpatel18@users.noreply.github.com>
2022-12-05 17:38:44 -0500

    added the stocks summary endpoint to the client libary:

---
+++ src/rest/stocks/summaries.ts
@@ -0,0 +1,66 @@
+// CF: https://polygon.io/docs/stocks/get_v2_aggs_ticker__stocksTicker__range__multiplier___timespan___from___to
+
+import { get, IPolygonQuery } from "../transport/request";
+
+export interface IBranding {
+    icon_url: string;
+    logo_url: string;
+}
+
+export interface IOptions {
+    contract_type: 'put' | 'call' | 'other';
+    exercise_style: 'american' | 'european' | 'bermudan';
+    expiration_date: string;
+    shares_per_contract: number;
+    strike_price: number;
+    underlying_ticker: string;
+}
+
+export interface ISession {
+    change: number;
+    change_percent: number;
+    close: number;
+    early_trading_change: number;
+    early_trading_change_percent: number;
+    high: number;
+    late_trading_change: number;
+    late_trading_change_percent: number;
+    low: number;
+    open: number;
+    pervious_close: number;
+    volume: number;
+
+}
+
+export interface ISummariesResults {
+  branding: IBranding;
+  market_status: string;
+  name: string;
+  option: IOptions;
+  price: number;
+  session: ISession;
+  ticker: string;
+  type: 'stock' | 'option' | 'forex' | 'crypto';
+}
+
+export interface ISummariesQuery extends IPolygonQuery {
+  'ticker.any_of'?: string;
+}
+
+export interface ISummaries {
+  request_id?: string;
+  status?: string;
+  results?: ISummariesResults[];
+}
+
+export const aggregates = async (
+  apikey: string,
+  apiBase: string,
+  query?: ISummariesQuery
+): Promise<ISummaries> =>
+  get(
+    `/v1/summaries`,
+    apikey,
+    apiBase,
+    query
+  );
