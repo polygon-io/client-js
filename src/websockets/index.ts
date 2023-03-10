149fc4462df0881d1fa82e5da014acf50966e3a9
Tyler Harnach <harnatc@gmail.com>
2023-03-10 10:21:57 -0600

    fix: updates websocket indices types and correctly exports it

--- src/websockets/index.ts
+++ src/websockets/index.ts
@@ -1,16 +1,19 @@
 import * as websocket from "websocket";
 import { getCryptoWebsocket } from "./crypto/index.js";
 import { getForexWebsocket } from "./forex/index.js";
+import { getIndicesWebsocket } from "./indices/index.js"
 import { getOptionsWebsocket } from "./options/index.js";
 import { getStocksWebsocket } from "./stocks/index.js";
 
 export * from "./forex/index.js";
+export * from "./indices/index.js";
 export * from "./stocks/index.js";
 export * from "./crypto/index.js";
 
 export interface IWebsocketClient {
   crypto: () => websocket.w3cwebsocket;
   forex: () => websocket.w3cwebsocket;
+  indices: () => websocket.w3cwebsocket;
   options: () => websocket.w3cwebsocket;
   stocks: () => websocket.w3cwebsocket;
 }
@@ -21,6 +24,7 @@ export const websocketClient = (
 ): IWebsocketClient => ({
   crypto: () => getCryptoWebsocket(apiKey, apiBase),
   forex: () => getForexWebsocket(apiKey, apiBase),
+  indices: () => getIndicesWebsocket(apiKey, apiBase),
   options: () => getOptionsWebsocket(apiKey, apiBase),
   stocks: () => getStocksWebsocket(apiKey, apiBase),
 });
--- src/websockets/indices/index.ts
+++ src/websockets/indices/index.ts
@@ -1,49 +1,23 @@
 import { getWsClient } from "../transport/index.js";
 import * as websocket from "websocket";
 
-// Stocks Aggregate:
-export interface IAggregateStockEvent {
+export interface IAggregateIndexEvent {
   ev: string; // Event Type ( A = Second Agg, AM = Minute Agg )
   sym: string; // Symbol Ticker
-  v: number; // Tick Volume
-  av: number; // Accumulated Volume ( Today )
-  op: number; // Todays official opening price
-  vw: number; // VWAP (Volume Weighted Average Price)
   o: number; // Tick Open Price
+  op: number; // Today's Open Price
   c: number; // Tick Close Price
   h: number; // Tick High Price
   l: number; // Tick Low Price
-  a: number; // Tick Average / VWAP Price
-  z: number; // Tick Average Trade Size
   s: number; // Tick Start Timestamp ( Unix MS )
   e: number; // Tick End Timestamp ( Unix MS )
 }
 
-export interface ITradeStockEvent {
+export interface IIndexValueEvent {
   ev: string; // Event Type
-  sym: string; // Symbol Ticker
-  x: string; // Exchange ID
-  i: number; // Trade ID
-  z: number; // Tape ( 1=A 2=B 3=C)
-  p: number; // Price
-  s: number; // Trade Size
-  c: number[]; // Trade Conditions
-  t: number; // Trade Timestamp ( Unix MS )
-}
-
-// Stocks QUOTE:
-export interface IQuoteStockEvent {
-  ev: string; // Event Type
-  sym: string; // Symbol Ticker
-  bx: string; // Bix Exchange ID
-  bp: number; // Bid Price
-  bs: number; // Bid Size
-  ax: string; // Ask Exchange ID
-  ap: number; // Ask Price
-  as: number; // Ask Size
-  c: number; // Quote Condition
-  t: number; // Quote Timestamp ( Unix MS )
-  z: number; // The tape (1 = NYSE, 2 = AMEX, 3 = Nasdaq)
+  val: string; // Value of the Index
+  T: string; // Assigned Ticker of the Index
+  t: number; // Timestamp ( Unix MS )
 }
 
 export const getIndicesWebsocket = (
