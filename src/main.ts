4c9851f4ea0a5dc41bf7302c28d94e0b6a60c601
mpatel18 <36168387+mpatel18@users.noreply.github.com>
2025-03-31 15:51:27 -0400

    poc for a auto-generated rest client using openapi genorator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 export * from "./rest/index.js";
 export * from "./websockets/index.js";
 
+import axios, { InternalAxiosRequestConfig } from 'axios';
 import restClient, { IRestClient } from "./rest/index.js";
 import websocketClient, { IWebsocketClient } from "./websockets/index.js";
+import { DefaultApi, Configuration } from "./generated-rest";
 
 export interface IPolygonClient {
   rest: IRestClient;
@@ -17,5 +19,20 @@ export const polygonClient = (
   rest: restClient(apiKey, restApiBase),
   websockets: websocketClient(apiKey, websocketApiBase),
 });
+
+export const generatedRestClient = (apikey: string): DefaultApi => {
+  // This function creates a generated REST client using the DefaultApi from the generated REST code
+  // Note: This does not include any custom interceptors or configurations
+  const config = new Configuration();
+  const SERVICE_BASE_URL = 'https://api.polygon.io'; // Fallback to default if not set
+
+  // Interceptor to add authorization token
+  axios.interceptors.request.use((requestConfig: InternalAxiosRequestConfig) => {
+    requestConfig.headers['Authorization'] = `Bearer ${apikey}`; // Use your API Key
+    return requestConfig;
+  });
+
+  return new DefaultApi(config, SERVICE_BASE_URL, axios);
+}
 
 export default polygonClient;
